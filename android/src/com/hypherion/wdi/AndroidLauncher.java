86b0187305030cd298f776f01dfc68018eab220c
AconLight <w.mosieski@interia.pl>
2016-12-29 21:43:57 +0100

    initial commit - set up project

---
+++ android/src/com/hypherion/wdi/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hypherion.wdi;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hypherion.wdi.WDIGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WDIGame(), config);
+	}
+}
